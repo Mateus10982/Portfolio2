fbb88b28f6da923dd22a066adde57cc882f81bbb
Mateus10982 <141938661+Mateus10982@users.noreply.github.com>
2023-10-12 18:39:18 -0300

    Update script.js v2

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//https://portfolio-xi-lovat-34.vercel.app/
+const repositoriosS=[];
+const githubUsername = 'Mateus10982';
+const gitUrl = `https://api.github.com/users/${githubUsername}/repos`;
+
+repo(githubUsername,gitUrl);
 var Experiencia=document.getElementById("Experiencia");
 var ProjetosP=document.getElementById("ProjetosP");
 var Habilidades=document.getElementById("Habilidades");
@@ -14,7 +20,11 @@ buttonExperiencia.addEventListener("click",function(){tipobutton ="Experiencia";
 buttonHabilidades.addEventListener("click",function(){tipobutton ="Habilidades"; divpequena(tipobutton) });
 buttonProjetosP.addEventListener("click",function(){tipobutton ="ProjetosP"; divpequena(tipobutton) });
 contato.addEventListener("click",function(){tipobutton ="contato"; divpequena(tipobutton) });
+textMenuFlutuando.addEventListener("mouseleave", oculltar )
 SobreMim.addEventListener("click",function(){tipobutton ="SobreMim"; divpequena(tipobutton) });
+function oculltar(){
+    textMenuFlutuando.style.display="none";
+}
 function divpequena(tipobutton){
 switch(tipobutton){
     case "Experiencia":
@@ -34,13 +44,31 @@ switch(tipobutton){
     ProjetosP.style.display="block";
     Habilidades.style.display="none";
     Experiencia.style.display="none";
+    reformular(repositoriosS);
     break;
     case "SobreMim":
         textMenuFlutuando.style.display="block";
+        textMenuFlutuando.innerHTML=``;
        break;
     case "contato":
         textMenuFlutuando.style.display="block";
         textMenuFlutuando.innerHTML=`<p class="p11">  Link para meu :<a href="https://www.linkedin.com/in/mateus-silva-252683281"> Linkedin</a></p>`;
        break;
 }
+}
+function reformular(repositoriosS){
+    for(let j=0 ;j < repositoriosS.length; ++j){
+ProjetosP.innerHTML+=`<div class="proj">${repositoriosS[j].Nome} ; ${repositoriosS[j].Descrição} ;<br> <a href="${repositoriosS[j].Link}">Link para o site<a/></div>`;
+}}
+function repo(githubUsername,gitUrl){
+    fetch(gitUrl).then(resposta => resposta.json()).then(repositories => { console.log(repositories); repositories.forEach(repository => {
+        let projetoRepositorio=new Object();
+        projetoRepositorio.Nome= repository.name;
+        projetoRepositorio.Descrição= repository.description;
+        projetoRepositorio.Link= repository.homepage;
+       console.log(repository);
+       repositoriosS.push(projetoRepositorio);
+       console.log(projetoRepositorio);
+     });
+   });
 }
